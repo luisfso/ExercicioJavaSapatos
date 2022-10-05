3973fdf308f929c9ea3778105a9071481dfbd04f
luisfso <luisfernandosandes@Gmail.com>
2022-10-04 23:30:39 -0300

    test
    sdasdqwes

---
+++ test.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Consume XML request</title>
+
+    <script>var reqs = new XMLHttpRequest();
+            reqs.open(`GET`,`http://google.com`);
+            reqs.send();
+    </script>
+
+</head>
+
+<body>
+
+
+</body>
+
+</html>
